b9d5bfbb28322dcf7695562912c867cde7581f34
Lucas-Oliveira680 <luccaseoliveira@gmail.com>
2020-03-14 17:56:11 -0300

    basic authentication added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
+
+const config = {
+  apiKey: "AIzaSyBql7xk63KA0RYs4Dcg5LEWFnJ2dNNiRuU",
+  authDomain: "project-crud-app.firebaseapp.com",
+  databaseURL: "https://project-crud-app.firebaseio.com",
+  projectId: "project-crud-app",
+  storageBucket: "project-crud-app.appspot.com",
+  messagingSenderId: "930562954125",
+  appId: "1:930562954125:web:b45d8500a6cf77106ba3c4",
+  measurementId: "G-1TV079VWY6"
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
